7ef1cdf3118fa386d5405db585f1275f7d6048ad
sheerryn <93917757+sheerryn@users.noreply.github.com>
2023-02-19 23:47:22 +0600

    Update sales.py

--- source-code/workflow-dag/sales.py
+++ source-code/workflow-dag/sales.py
@@ -4,9 +4,9 @@ from pyspark.sql.types import LongType, StringType, StructField, StructType, Dat
 import time
 
 DAYS_DIFF = 723
-BUCKET_NAME = 'elevated-dynamo-bucket'
-PROJECT_ID = 'elevated-dynamo-370709'
-DATASET_ID = 'dataproc'
+BUCKET_NAME = '${GCP_PROJECT_ID}-bucket'
+PROJECT_ID = $(gcloud config list --format 'value(core.project)')
+DATASET_ID = 'project_bq'
 
 spark_session = SparkSession.builder \
     .appName("JSONtoBigQuery") \
@@ -15,12 +15,12 @@ spark_session = SparkSession.builder \
 
 start = time.time()
 df_with_schema = spark_session.read.format("csv") \
-    .load(f"gs://elevated-dynamo-bucket/supermarket_sales - Sheet1.csv")
+    .load(f"gs://${GCP_PROJECT_ID}-bucket/supermarket_sales.csv")
 print(f"Read data execution time without specifying schema: {time.time() - start} seconds")
 
 df_with_schema.write.format('bigquery') \
-    .option("temporaryGcsBucket", 'dataprocbucket24123') \
-    .option('table', 'elevated-dynamo-370709.dataproc.sales' ) \
+    .option("temporaryGcsBucket", 'dataprocbucket241233') \
+    .option('table', 'project_bq.dataproc.sales' ) \
     .save()
 
 spark_session.stop()
